093d60c19f2c019a90a0e5404f1fee2922448660
Max Shapira <maxshapira9@gmail.com>
2020-06-25 10:48:59 +0300

    Fixing errors

--- script.js
+++ script.js
@@ -5,13 +5,16 @@ const input = document.getElementById('userinput');
 const ul = document.querySelector('ul');
 
 function inputLength() {
+  // @ts-ignore
   return input.value.length;
 }
 
 function createListElement() {
   const li = document.createElement('li');
+  // @ts-ignore
   li.appendChild(document.createTextNode(input.value));
   ul.appendChild(li);
+  // @ts-ignore
   input.value = '';
 }
 
@@ -36,11 +39,3 @@ input.addEventListener('keypress', addListAfterKeypress);
 // -----------------------------------------------------------------------------
 // Exercise
 // -----------------------------------------------------------------------------
-
-const itemDone = document.getElementsByClassName('.done');
-
-function markItemDone() {
-  const clickItem = document.getElementsByTagName(li).onclick;
-}
-
-itemDone.addEventListener('click', addListAfterClick);
